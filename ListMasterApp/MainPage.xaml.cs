dd8c60f61f9985495a3b49253f798d5416deaad6
andreivercosa <andreivercosa@gmail.com>
2019-03-13 21:48:18 -0400

    v1

---
+++ ListMasterApp/MainPage.xaml.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace ListMasterApp
+{
+    public partial class MainPage : ContentPage
+    {
+        int i = 0;
+        List<string> lista_de_valor = new List<string>();
+        void Handle_Clicked(object sender, System.EventArgs e)
+        {
+            Navigation.PushAsync(new PageMinhaLista(lista_de_valor));
+
+        }
+
+        void Handle_Clicked_1(object sender, System.EventArgs e)
+        {
+            lista_de_valor.Add("Item " + i);
+            i++;
+        }
+
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ ListMasterApp/PageMinhaLista.xaml.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace ListMasterApp
+{
+    public partial class PageMinhaLista : ContentPage
+    {
+        List<string> lista_conteudo = new List<string>(new string []
+        {
+            "Item 01",
+            "Item 02",
+            "Item 03"
+        });
+
+        public PageMinhaLista(List<string> listaDeOrigemDesconhecida)
+        {
+            InitializeComponent();
+            minhaLista.ItemsSource = listaDeOrigemDesconhecida;
+        }
+    }
+}
